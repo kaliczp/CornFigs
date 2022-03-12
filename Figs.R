a409196e6ae72e4206868f28cc45fb2a7a99956e
Péter Kalicz <kaliczp@gmail.com>
2022-03-12 06:05:02 +0100

    Boundaries from gadm
    Boundaries downloaded, imported and the megye map is plotted

--- Figs.R
+++ Figs.R
@@ -1,2 +1,5 @@
-read.table("Wheat.txt", sep = ";", head = TRUE)
-
+# http://www.gadm.org/
+wheat <- read.table("Wheat.txt", sep = ";", head = TRUE)
+library(sf)
+Hungary <- st_read("gadm/gadm40_HUN_1.shp")
+plot(Hungary[,"NAME_1"])
