7cfa2a76f49c12a35d8d875e2d6de0f32b0f0450
Péter Kalicz <kaliczp@gmail.com>
2022-03-12 06:26:45 +0100

    Collect regions to different objects
    Group megyes into region groups with st_union()

--- Figs.R
+++ Figs.R
@@ -3,3 +3,41 @@ wheat <- read.table("Wheat.txt", sep = ";", head = TRUE)
 library(sf)
 Hungary <- st_read("gadm/gadm40_HUN_1.shp")
 plot(Hungary[,"NAME_1"])
+
+megye_names <- Hungary[,"NAME_1", drop = TRUE]
+
+Mountains <- st_union(Hungary[
+    megye_names == "Borsod-Abaúj-Zemplén" |
+    megye_names == "Nógrád" |
+    megye_names == "Heves"
+   ,"NAME_1"]
+    )
+
+Transdanubia_Hills <- st_union(Hungary[
+    megye_names == "Komárom-Esztergom" |
+    megye_names == "Pest" |
+    megye_names == "Budapest" |
+    megye_names == "Vas" |
+    megye_names == "Veszprém" |
+    megye_names == "Zala"
+   ,"NAME_1"]
+    )
+
+Hungarian_Plain <- st_union(Hungary[
+    megye_names == "Szabolcs-Szatmár-Bereg" |
+    megye_names == "Hajdú-Bihar" |
+    megye_names == "Jász-Nagykun-Szolnok" |
+    megye_names == "Bács-Kiskun" |
+    megye_names == "Békés" |
+    megye_names == "Csongrád"
+   ,"NAME_1"]
+    )
+
+Transdanubia_Lowlands <- st_union(Hungary[
+    megye_names == "Fejér" |
+    megye_names == "Gyor-Moson-Sopron" |
+    megye_names == "Somogy" |
+    megye_names == "Baranya" |
+    megye_names == "Tolna"
+   ,"NAME_1"]
+    )
