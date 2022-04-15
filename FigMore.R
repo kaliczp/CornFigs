bccb343da63ca067205790b05f9e19b72a933c9e
Péter Kalicz <kaliczp@gmail.com>
2022-04-15 14:00:47 +0200

    More accurate names
    Now the object name is more understandeable and R2 is used as as name of the values.

--- FigMore.R
+++ FigMore.R
@@ -2,17 +2,17 @@ library(sf)
 library(tmap)
 tmap_mode("plot")
 
-T2250 <- st_sf(wheat = wheat.transform[,1], HunRegions)
+T2250 <- st_sf(R2 = wheat.transform[,1], HunRegions)
 T2250$TimeWin <- "Tmean 1922-1950"
 
-T5180 <- st_sf(wheat = wheat.transform[,2], HunRegions)
+T5180 <- st_sf(R2 = wheat.transform[,2], HunRegions)
 T5180$TimeWin <- "Tmean 1951-1980"
 
-T8110 <- st_sf(wheat = wheat.transform[,3], HunRegions)
+T8110 <- st_sf(R2 = wheat.transform[,3], HunRegions)
 T8110$TimeWin <- "Tmean 1981-2010"
 
-Wheat <- rbind(T2250, T5180, T8110)
+Fig1 <- rbind(T2250, T5180, T8110)
 
-tm_shape(Wheat) +
-    tm_polygons("wheat") +
+tm_shape(Fig1) +
+    tm_polygons("R2") +
     tm_facets("TimeWin", nrow = 1)
