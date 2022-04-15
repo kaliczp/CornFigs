0bf1dc0ef58855f9ad21e50503b6ffe504a0953f
Péter Kalicz <kaliczp@gmail.com>
2022-04-15 12:05:50 +0200

    Multiple maps with tmap + grid

--- FigMore.R
+++ FigMore.R
@@ -1,6 +1,21 @@
+library(grid)
 library(tmap)
 tmap_mode("plot")
 
-tm_shape(wheat.sf) +
-    tm_polygons(c("Temp1922-1950", "Temp1951-1980", "Temp1981-2010")) +
-    tm_facets(ncol = 3) + tm_layout(legend.show = FALSE)
+T2250 <- tm_shape(wheat.sf) +
+    tm_polygons("Temp1922-1950") +
+    tm_layout(legend.show = FALSE)
+
+T5180 <- tm_shape(wheat.sf) +
+    tm_polygons("Temp1951-1980") +
+    tm_layout(legend.show = FALSE)
+
+T8110 <- tm_shape(wheat.sf) +
+    tm_polygons("Temp1981-2010") +
+    tm_layout(legend.show = TRUE, legend.position = c("right", "bottom"))
+
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(1, 3)))
+print(T2250, vp = viewport(layout.pos.col = 1))
+print(T5180, vp = viewport(layout.pos.col = 2))
+print(T8110, vp = viewport(layout.pos.col = 3))
