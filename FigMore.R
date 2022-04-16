249be5aa56d3b45a1c9eefbfc9f7e21d31fc4796
Péter Kalicz <kaliczp@gmail.com>
2022-04-16 13:11:11 +0200

    Save result as a png file

--- FigMore.R
+++ FigMore.R
@@ -24,7 +24,10 @@ Tm8110$TimeWin <- "Tmax 1981-2010"
 Fig1 <- rbind(T2250, T5180, T8110, Tm2250, Tm5180, Tm8110)
 Fig1$TimeWin <- factor(Fig1$TimeWin, levels = unique(Fig1$TimeWin),  ordered=TRUE)
 
-tm_shape(Fig1) +
+Fig1.tm <- tm_shape(Fig1) +
     tm_fill("R2", breaks = seq(-0.8,0.8,by=0.1)) +
     tm_borders() +
+    tm_layout(legend.outside = TRUE) + 
     tm_facets("TimeWin", nrow = 2)
+
+tmap_save(Fig1.tm, filename = "Fig1.png", height = 4, width = 5, asp = 0)
