d26f03c3785bff2443f2b868e8b75003a1908208
Péter Kalicz <kaliczp@gmail.com>
2022-06-22 13:56:00 +0200

    First version of FigS1

---
+++ FigS1.R
@@ -0,0 +1,40 @@
+library(sf)
+library(tmap)
+tmap_mode("plot")
+
+TPrec8110 <- st_sf(R2 = wheat2.transform[,1], HunRegions)
+TPrec8110$TimeWin <- "TmeanPrec"
+
+TPrecPET8110 <- st_sf(R2 = wheat2.transform[,2], HunRegions)
+TPrecPET8110$TimeWin <- "TmeanPrecPET"
+
+TTmean8110 <- st_sf(R2 = wheat2.transform[,3], HunRegions)
+TTmean8110$TimeWin <- "Tmean"
+
+Fig2 <- rbind(TPrec8110, TPrecPET8110, TTmean8110)
+Fig2$TimeWin <- factor(Fig2$TimeWin, levels = unique(Fig2$TimeWin),  ordered=TRUE)
+
+tmap_options(legend.outside = TRUE, legend.stack = "horizontal", legend.outside.position = "bottom", legend.width = 1)
+
+, legend.outside.size = 2)
+
+tmap_options_reset()
+
+Fig2.tm <- tm_shape(Fig2) +
+    tm_fill("R2", breaks = seq(-0.8,0.8,by=0.1)) +
+    tm_borders() +
+    tm_layout(title = c(expression(plain(Prec)), expression(plain(Prec-PET)), expression(plain(Tmean[Jan-March]))),
+              title.position = c("center","top"), title.size = 0.9,
+              panel.labels = c("",expression(bold(Tmean[May-July] + ldots)),""),
+              panel.label.size = 1,
+              legend.show = FALSE,
+              frame = FALSE,
+              frame.lwd = NA,
+              panel.label.bg.color = NA,
+              asp = 0) +
+    tm_facets("TimeWin", nrow = 1)
+
+tmap_save(Fig2.tm, filename = "Fig2.png", height = 35/25.4, width = 84/25.4, asp = 0)
+tmap_save(Fig2.tm, filename = "Fig2.eps", height = 35/25.4, width = 84/25.4, asp = 0)
+
+
