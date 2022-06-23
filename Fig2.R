0455587eb315123083bcfa489d1f3803f63266e5
Péter Kalicz <kaliczp@gmail.com>
2022-06-23 14:44:18 +0200

    Fig 2 size and labels modified according to other figs
    Fig 2 titles are merged.
    
    Size is modified to one page wide figure.
    
    Data structure reworked similar to others.

--- Fig2.R
+++ Fig2.R
@@ -3,16 +3,20 @@ library(tmap)
 tmap_mode("plot")
 
 TPrec8110 <- st_sf(R2 = wheat2.transform[,1], HunRegions)
-TPrec8110$TimeWin <- "TmeanPrec"
+TPrec8110$TimeWin <- "1981–2020"
+TPrec8110$Param <- "TmeanPrec"
 
 TPrecPET8110 <- st_sf(R2 = wheat2.transform[,2], HunRegions)
-TPrecPET8110$TimeWin <- "TmeanPrecPET"
+TPrecPET8110$TimeWin <- "1981–2020"
+TPrecPET8110$Param <- "TmeanPrecPET"
 
 TTmean8110 <- st_sf(R2 = wheat2.transform[,3], HunRegions)
-TTmean8110$TimeWin <- "Tmean"
+TTmean8110$TimeWin <- "1981–2020"
+TTmean8110$Param <- "Tmean"
 
 Fig2 <- rbind(TPrec8110, TPrecPET8110, TTmean8110)
 Fig2$TimeWin <- factor(Fig2$TimeWin, levels = unique(Fig2$TimeWin),  ordered=TRUE)
+Fig2$Param <- factor(Fig2$Param, levels = unique(Fig2$Param),  ordered=TRUE)
 
 tmap_options(legend.outside = TRUE, legend.stack = "horizontal", legend.outside.position = "bottom", legend.width = 1)
 
@@ -23,18 +27,19 @@ tmap_options_reset()
 Fig2.tm <- tm_shape(Fig2) +
     tm_fill("R2", palette = "Blues", breaks = seq(0,0.8,by=0.1)) +
     tm_borders() +
-    tm_layout(title = c(expression(plain(Prec)), expression(plain(Prec-PET)), expression(plain(Tmean[Jan-March]))),
-              title.position = c("center","top"), title.size = 0.9,
-              panel.labels = c("",expression(bold(Tmean[May-July] + ldots)),""),
-              panel.label.size = 1,
+    tm_layout(panel.labels = c(expression(bold(Tmean[May-July]) + plain(Prec)),
+                               expression(bold(Tmean[May-July]) + plain(Prec-PET)),
+                               expression(bold(Tmean[May-July]) + plain(Tmean[Jan-March]))
+                               ),
+              panel.label.size = 0.7,
               legend.show = FALSE,
               frame = FALSE,
               frame.lwd = NA,
               panel.label.bg.color = NA,
               asp = 0) +
-    tm_facets("TimeWin", nrow = 1)
+    tm_facets("Param", nrow = 1)
 
-tmap_save(Fig2.tm, filename = "Fig2.png", height = 35/25.4, width = 84/25.4, asp = 0)
-tmap_save(Fig2.tm, filename = "Fig2.eps", height = 35/25.4, width = 84/25.4, asp = 0)
+tmap_save(Fig2.tm, filename = "Fig2.png", height = 29/25.4, width = 174/25.4, asp = 0)
+tmap_save(Fig2.tm, filename = "Fig2.eps", height = 29/25.4, width = 174/25.4, asp = 0)
 
 
