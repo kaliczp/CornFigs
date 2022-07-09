00a79e40c43ffae5673171fdea2e98db218563c8
Péter Kalicz <kaliczp@gmail.com>
2022-07-09 12:23:44 +0200

    Refine bounding box of the overview map

--- FigLocation.R
+++ FigLocation.R
@@ -1,7 +1,7 @@
 library(tmap)
 data("World")
 
-tm_shape(World, projection = 3035, bbox = st_bbox(c(xmin = 16 - 60, xmax = 16 + 60, ymax = 46 + 40, ymin = 46 - 40), crs = 4326) ) +
+tm_shape(World, projection = 3035, bbox = st_bbox(c(xmin = 19.3 - 70, xmax = 19.3 + 70, ymax = 47 + 40, ymin = 47 - 40), crs = 4326) ) +
     tm_polygons("white") +
     tm_graticules()
 
