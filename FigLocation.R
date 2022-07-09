bbd4087917e6b5be91852f68ac9bd37a7335c9a3
Péter Kalicz <kaliczp@gmail.com>
2022-07-09 14:00:11 +0200

    Custom palette applied to overview map

--- FigLocation.R
+++ FigLocation.R
@@ -4,8 +4,10 @@ data("World")
 World$Hun  <-  0
 World[World$name == "Hungary","Hun"]  <-  1
 World$Hun  <-  as.logical(World$Hun)
+Palette <- get_brewer_pal("PuOr", n = 3)
+Palette <- Palette[c(2,1,3)]
 
 tm_shape(World, projection = 3035, bbox = st_bbox(c(xmin = 19.3 - 70, xmax = 19.3 + 70, ymax = 47 + 40, ymin = 47 - 40), crs = 4326) ) +
-    tm_polygons("Hun") +
+    tm_polygons("Hun", palette = Palette, colorNULL = Palette[3]) +
     tm_graticules()
 
