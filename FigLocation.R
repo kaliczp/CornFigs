6a6059b79220c08529da0378eeb90a0da701134e
Péter Kalicz <kaliczp@gmail.com>
2022-07-09 12:29:46 +0200

    Indicate area of interest

--- FigLocation.R
+++ FigLocation.R
@@ -1,7 +1,11 @@
 library(tmap)
 data("World")
 
+World$Hun  <-  0
+World[World$name == "Hungary","Hun"]  <-  1
+World$Hun  <-  as.logical(World$Hun)
+
 tm_shape(World, projection = 3035, bbox = st_bbox(c(xmin = 19.3 - 70, xmax = 19.3 + 70, ymax = 47 + 40, ymin = 47 - 40), crs = 4326) ) +
-    tm_polygons("white") +
+    tm_polygons("Hun") +
     tm_graticules()
 
